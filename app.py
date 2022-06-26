ad308921e2c127898e014fc28ee9e7863a360493
Emmanuel Kamanda <56160669+Emmanuel1255@users.noreply.github.com>
2022-06-26 01:56:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask
 from flask_restful import Resource, Api
 from bs4 import BeautifulSoup
 import urllib.request
-from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 class curency_converter(Resource):
     def get(self):
